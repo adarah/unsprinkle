1e91e875c363de585bd2c58c0befeadceaa708a3
Lucas Harada <lucasyharada@gmail.com>
2024-11-17 11:05:09 -0300

    exercise 3 after seeing solution

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ import Image from "../Image";
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" alt="A cat staring deeply at your soul."/>
+      <HeroImage src="/images/hero-img.jpg" alt="An example of a photo you can acquire from this website"/>
       <Swoop src="/swoop.svg" alt="" />
     </Wrapper>
   );
